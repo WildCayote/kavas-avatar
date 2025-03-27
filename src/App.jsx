c117e3cd7f6a832c19dabe44ca9a86c7fdc9ea70
fraol7 <fraolmulugeta17@gmail.com>
2025-03-27 11:25:39 +0300

    Feat: Added kavas background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { TextureLoader } from "three";
+import { useLoader } from "@react-three/fiber";
 import { UI } from "./components/UI";
 
 function App() {
   return (
     <>
       <Canvas shadows>
-        <color attach="background" args={["#ececec"]} />
+        <primitive attach="background" object={useLoader(TextureLoader, "/public/kavas-bg.png")} />
         <Experience />
       </Canvas>
       <UI />
